cc8b1dfdb0130623881b76cb8c9096efc808bbbf
joeyAddy <79617326+joeyAddy@users.noreply.github.com>
2023-05-18 22:56:28 -0700

    changed CORS origin to *

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "https://k-track.netlify.app",
+    origin: "*",
     // "https://k-track.netlify.app",
     methods: "GET,POST,PATCH,DELETE",
     // credentials: true,
